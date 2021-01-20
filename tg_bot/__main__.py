ef87b7cf86fc957ae6138ec9c1712eafd7f7f7ef
Mr_Perfect_purushottam <77604098+Mr-PerfectPURUSHOTTAM@users.noreply.github.com>
2021-01-20 08:54:10 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -49,7 +49,7 @@ Commands Available In **Main Menu**:
 And the Following : 
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/3ffcca74b8f564f7883fc.jpg"
+TECHNO_IMG = "https://telegra.ph/file/00ffcbfdb833b24994fa9.png"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
